8764e62e58b05b7b2e110781f3efeafdc80188d2
Anish Gulati <anish.gulati@research.iiit.ac.in>
2018-02-22 17:39:12 +0530

    Fixed minor bugs

--- team4.py
+++ team4.py
@@ -109,36 +109,37 @@ class Team4:
 			return ans
 
 	def heuristic(self, board, old_move):
-		
 		goodness = 0
 		goodness += self.calc_single_blocks(board, old_move)
 		goodness += self.calc_as_whole(board, old_move)
 		return goodness
 
-
 	def calc_single_blocks(self, board, old_move):
-		
 		block_goodness = 0
 		for i in xrange(4):
 			for j in xrange(4):
 				block_goodness += self.calc_per_block(board, old_move, i, j)
+                return block_goodness
 
 	def calc_per_block(self, board, old_move, block_x, block_y):
 
 		# For checking how good a row/col is
-		row_weight = [10 10 10 10]
-		col_weight = [10 10 10 10]
+		row_weight = [10, 10, 10, 10]
+		col_weight = [10, 10, 10, 10]
 		for i in xrange(4):
 			for j in yrange(4):
 				mapping_val = self.mapping[board.board_status[4*block_x+i][4*block_y+j]]
-				# row_weight += mapping_val * self.cell_weight			probably will only help in case of overall block
-				row_weight += mapping_val * 10
-				col_weight += mapping_val * 10
+                    # Yes, the below line will help in the overall case
+                                # row_weight += mapping_val * self.cell_weight			probably will only help in case of overall block
+                                if(row_weight[i] == 10):
+                                        row_weight += mapping_val * 10
+                                if(col_weight[j] == 10):
+                                        col_weight += mapping_val * 10
 				if (mapping_val == -1):
 					row_weight[i] = 0
 					col_weight[j] = 0
-				row_weight *= 3
-				col_weight *= 3
+				row_weight[i] *= 3
+				col_weight[j] *= 3
 
 		# For checking how good diamond state is
 
@@ -146,14 +147,16 @@ class Team4:
 	def calc_as_whole(self, board):
 
 		# For checking how good a row/col is
-		row_weight = [10 10 10 10]
-		col_weight = [10 10 10 10]
+		row_weight = [10, 10, 10, 10]
+		col_weight = [10, 10, 10, 10]
 		for i in xrange(4):
 			for j in yrange(4):
 				mapping_val = self.mapping[board.block_status[i][sj]]
 				row_weight += mapping_val * self.cell_weight			# probably will only help in case of overall block
-				row_weight += mapping_val * 10
-				col_weight += mapping_val * 10
+                                if(row_weight[i] == 10):
+                                        row_weight += mapping_val * 10
+                                if(col_weight[j] == 10):
+                                        col_weight += mapping_val * 10
 				if (mapping_val == -1):
 					row_weight[i] = 0
 					col_weight[j] = 0
